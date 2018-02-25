e3a8439375b7bd40b9fc2b83642c88e26d0d73a9
hecter <hecter654321@gmail.com>
2018-02-25 23:25:33 +0800

    pass testcase: testSample by for statement

--- Best Time to Buy and Sell Stock.py
+++ Best Time to Buy and Sell Stock.py
@@ -5,11 +5,19 @@ class unitest(unittest.TestCase):
         Input = []
         Output = 0
         self.assertEqual(Solution().maxProfit(Input),Output)
+    def testSample(self):
+        Input = [7, 1, 5, 3, 6, 4]
+        Output = 5
+        self.assertEqual(Solution().maxProfit(Input),Output)
 
 class Solution():
     def maxProfit(self, prices):
-        if prices == []:
-            return 0
+        ans = 0
+        low = prices[0]
+        for i in prices:
+            ans = max(ans,(i-low))
+            low = min(i,low)
+        return ans
 
 if __name__ == '__main__':
     unittest.main()
